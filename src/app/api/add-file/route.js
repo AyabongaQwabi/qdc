37a7088cdfa2ab0a017740aa7b8930708b70bd02
Ayabonga Qwabi <aya@simply.co.za>
2025-01-03 01:57:47 +0200

    fix api

---
+++ src/app/api/add-file/route.js
@@ -0,0 +1,28 @@
+import { sendNewFile } from '@/app/notifications';
+import clientPromise from '../../lib/mongo';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req) {
+  try {
+    console.log('adding file');
+    const client = await clientPromise;
+    const json = await req.json();
+    console.log(json);
+    const db = client.db(process.env.SELECTED_DB);
+    await db.collection('files').insertOne(json);
+    sendNewFile(json);
+    return new NextResponse(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (e) {
+    console.log(e);
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+}
